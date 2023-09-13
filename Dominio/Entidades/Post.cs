9e0c4df417c276bbbdeb3cc1a8db598b850e6e45
Sr.Lechuga <jonattanlima@gmail.com>
2023-09-12 23:52:31 -0300

    Definition of class 'Post'

--- Dominio/Entidades/Post.cs
+++ Dominio/Entidades/Post.cs
@@ -2,5 +2,83 @@
 {
     public class Post : Publicacion
     {
+        #region Atributos
+        private string _image;
+        private bool _privado;
+        private List<Comentario> _comentarios;
+        private bool _censurado;
+        #endregion
+
+        #region Propiedades
+        public string Image
+        {
+            get { return _image; }
+            set { _image = value; }
+        }
+        public bool Privado
+        {
+            get { return _privado; }
+            set { _privado = value; }
+        }
+        public List<Comentario> Comentarios
+        {
+            get { return _comentarios; }
+        }
+        public bool Censurado
+        {
+            get { return _censurado; }
+        }
+        #endregion
+
+        #region Constructores
+        /// <summary>
+        /// Permite crear un post con una imagen y definir si el post sera privado.
+        /// <para><i>Nota: Al crearse un post, comienza sin ningún comentario</i></para>
+        /// </summary>
+        public Post()
+        {
+            _image = string.Empty;
+            _comentarios = new List<Comentario>();
+        }
+
+        /// <summary>
+        /// Permite crear un post con una imagen y definir si el post sera privado.
+        /// <para><i>Nota: Al crearse un post, comienza sin ningún comentario</i></para>
+        /// </summary>
+        /// <param name="image">
+        /// Guarda la imagen del post
+        /// <para><i><b>Nota: </b>Actualmente solo se guarda el nombre de la imagen.</i></para>
+        /// </param>
+        /// <param name="privado">
+        /// Permite definir si un post es publico(<c>false</c>) o privado(<c>true</c>)
+        /// </param>
+        public Post(string image, bool privado) : base()
+        {
+            image ??= string.Empty;
+            _image = image;
+            _privado = privado;
+
+            //Al crear una post el mismo no deberia tener comentarios ni deberia estar censurado
+            _comentarios = new List<Comentario>();
+            _censurado = false;
+        }
+
+        #endregion
+
+        #region Metodos
+        /// <summary>
+        /// Permite agregar un <c>Comentario</c> a la lista de comentarios
+        /// </summary>
+        /// <param name="comentario">
+        /// <c>Comentario</c> a agregar al <c>Post</c>
+        /// </param>
+        public void AgregarComentario(Comentario comentario)
+        {
+            //TODO: Logica para agregar un comentario al post
+        }
+        #endregion
+
+        #region Metodos sobreescritos
+        #endregion
     }
 }
