14f23c16d7620478b1ed4c4a5fc3d3a1a24b320d
Sr.Lechuga <jonattanlima@gmail.com>
2023-10-01 18:09:50 -0300

    Feature: Validar fecha de nacimiento en miembro

--- Dominio/Entidades/Miembro.cs
+++ Dominio/Entidades/Miembro.cs
@@ -80,7 +80,9 @@
         /// <exception cref="Exception"></exception>
         public void ValidarFechaNacimiento()
         {
-            //TODO: Logica de validar fecha nacimiento
+            if (_fechaNacimiento > DateTime.Today)
+                throw new Exception("La fecha de nacimiento debe ser menor a la fecha actual");
+            //TODO: Create an custom exception for date of birth 
         }
 
         /// <summary>
