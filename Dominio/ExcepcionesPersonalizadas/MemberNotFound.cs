397c04c9fc209dc5406f779f1997e8fc011ebf49
Sr.Lechuga <jonattanlima@gmail.com>
2023-10-05 12:43:04 -0300

    Feature: Exepcion personalizada para miembro no encontrado

---
+++ Dominio/ExcepcionesPersonalizadas/MemberNotFound.cs
@@ -0,0 +1,17 @@
+﻿using Dominio.Entidades;
+
+namespace Dominio.ExcepcionesPersonalizadas
+{
+    [Serializable]
+    internal class MemberNotFound : Exception
+    {
+        public Miembro Miembro { get; }
+
+        public MemberNotFound(string mensaje) { }
+
+        public MemberNotFound(string mensaje, Miembro miembro) 
+        {
+            Miembro = miembro;
+        }
+    }
+}
