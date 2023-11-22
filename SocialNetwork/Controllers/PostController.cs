bec96e06878764d113bd2f3ad413117a2d7d7f4b
Sr.Lechuga <jonattanlima@gmail.com>
2023-11-21 21:44:52 -0300

    Update: Post to wonderland

---
+++ SocialNetwork/Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Controllers
+{
+    public class PostController : Controller
+    {
+        Dominio.SocialNetwork _sistema = Dominio.SocialNetwork.Instancia;
+
+        public IActionResult Wonderland()
+        {
+            ViewBag.Posts = _sistema.Posteos;
+            return View();
+        }
+
+        public IActionResult Buscar()
+        {
+            return View();
+        }
+
+    }
+}
